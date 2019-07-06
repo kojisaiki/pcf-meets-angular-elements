2e1739660c7803e6ecf373874169888cb8208bc9
Koji Saiki <saikou9901@gmail.com>
2019-07-06 17:30:08 +0900

    [not work] prepare for angular elements

--- angular-elements-app/src/app/app.component.ts
+++ angular-elements-app/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-elements-app';
+  showMessage() {
+    const hello = document.getElementById('hello');
+    hello.innerHTML = '<app-hello-world></app-hello-world>';
+  }
 }
